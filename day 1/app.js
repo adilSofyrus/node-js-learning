fb232e2e2bc920b17807d6bfe07592c24072fe75
adilSofyrus <adil.ayoub@sofyrus.com>
2025-02-12 12:41:08 +0530

    importing as obj

--- day 1/app.js
+++ day 1/app.js
@@ -1,5 +1,5 @@
 require("./sep_module.js");
-const calculateSum =  require("./sum.js");
+const obj =  require("./sum.js");
 var name = "adil ayoub";
 
 
@@ -8,7 +8,9 @@ var  a = 10;
 var b = 20;
 
 
-calculateSum(a,b)
+obj.calculateSum(a,b)
+
+console.log(obj.x);
 
 console.log(name);
 console.log(a+b);
--- day 1/sum.js
+++ day 1/sum.js
@@ -1,6 +1,6 @@
 console.log("inside the sum module ");
 
-
+var x = "Helo World";
 // Module protects their veriable and functions from leacking 
 
 function calculateSum(a,b){
@@ -10,4 +10,8 @@ function calculateSum(a,b){
   console.log(sum);
 }
 
-module.exports = calculateSum;
+module.exports =
+// we wrap it inside an obj 
+{ x: x,  // method and property x: x
+ calculateSum : calculateSum,
+};
