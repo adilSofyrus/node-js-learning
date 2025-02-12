0113fcaaa8b78d889549d3381c41a62e01dacf4b
adilSofyrus <adil.ayoub@sofyrus.com>
2025-02-12 12:17:46 +0530

    know the global obj and globalthis

---
+++ day 1/app.js
@@ -0,0 +1,23 @@
+var name = "adil ayoub";
+
+
+var  a = 10;
+
+var b = 20;
+
+
+
+console.log(name);
+console.log(a+b);
+
+
+//   console.log(global); // node base global object
+
+//   console.log(this); // Empty object
+
+
+
+console.log(globalThis); // standardised way to access global object
+
+
+console.log(globalThis === global); // true
