dd25d7a954b3e7ad912dddcabbfb6bc1a1c7a9ba
adilSofyrus <adil.ayoub@sofyrus.com>
2025-02-13 19:40:19 +0530

    add sync.js with multiplication function and logging

---
+++ day 4/sync.js
@@ -0,0 +1,14 @@
+
+var a = 1078698;
+
+var b = 20986;
+
+
+function  funMultiply(a, b) {
+    const result = a * b;
+    return result;
+}
+
+let c = funMultiply(a, b);
+
+console.log("Multiplication of a and b is: ", c);
